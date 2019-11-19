353d33ff0d1fe7795441bc88385139e1b57e1317
edensalansky <eden.salansky@gmail.com>
2019-11-19 21:39:45 +0200

    hello

---
+++ src/Episode.cpp
@@ -0,0 +1,4 @@
+//
+// Created by edensal@wincs.cs.bgu.ac.il on 19/11/2019.
+//
+
--- src/Session.cpp
+++ src/Session.cpp
@@ -20,26 +20,60 @@ User* activeUser;
         json j;
         i >> j;
         //movies
-        for (int i = 0; i < j.size(); i++) {
-            long id = i;
+        long id=0 ;
+        for (int i = 0; i < j["movies"].size(); i++) {
+
+            id++;
+            //cout<<id<<". ";
             const std::string &name = j["movies"][i]["name"];
-            cout << name;
+            // cout << name <<" ";
             int length = j["movies"][i]["length"];
-            cout << length;
-            const std::vector<std::string> &tags = j["movies"][i]["tags"];
-            string s="[";
-            for (vector<string>::const_iterator it = tags.begin(); it !=  tags.end(); it++)
-                s=s+(*it)+",";
-            cout << s <<"]"<<endl;
-          //  content.push_back(new Watchabl(i, length, tags));
-
-
+            // cout << length<<" minutes ";
+            const std::vector<string> &tags = j["movies"][i]["tags"];
+            // string s="[";
+            //   for (vector<string>::const_iterator it = tags.begin(); it !=  tags.end(); it++)
+            //     s=s+(*it)+", ";
+            //  cout <<s.substr(0,s.length()-2)<<"]"<< endl;
+           // content.push_back(new Movie(i, name, length, tags));
         }
-  }
+
+          for (int i = 0; i < j["tv_series"].size(); i++) {
+              id++;
+              const std::string &seriesName = j["tv_series"][i]["name"];
+              int length = j["tv_series"][i]["episode_length"];
+              const std::vector<int> &seasons = j["tv_series"][i]["seasons"];
+              int season = 0;
+              const std::vector<std::string> &tags = j["tv_series"][i]["tags"];
+              for (vector<int>::const_iterator it = seasons.begin(); it != seasons.end(); it++) {//
+                  season++;
+                  for (int episode = 1; episode <= *it; episode++) {
+                      //  content.push_back(new Episode( id,  seriesName, length,  season,  episode , tags);//
+                      cout << id << ". ";
+                      cout << seriesName << " ";
+                      cout << length << " minutes ";
+                      cout << "S"<<season << "E"<< episode <<" ";
+                       string s="[";
+                      for (vector<string>::const_iterator it = tags.begin(); it !=  tags.end(); it++)
+                           s=s+(*it)+", ";
+                       cout <<s.substr(0,s.length()-2)<<"]"<< endl;
+                      id++;
+
+                  }
+              }
+          }
 
 
 
-    Session::~Session() {
+              //  content.push_back(new Movie(i,name,length, tags));
+
+
+
+          }
+
+
+
+
+    Session::~Session(){
 
     }
 
