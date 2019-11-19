1d074dc937b67eb0c0e020d1e616d262c75de8f0
edensalansky <eden.salansky@gmail.com>
2019-11-19 20:25:04 +0200

    hello

--- src/Session.cpp
+++ src/Session.cpp
@@ -8,10 +8,14 @@
 #include <fstream>
 
 using json = nlohmann::json;
+std::vector<Watchable*> content;
+std::vector<BaseAction*> actionsLog;
+std::unordered_map<std::string,User*> userMap;
+User* activeUser;
 
 
+    Session::Session(const std::string &configFilePath):content(),actionsLog (),userMap(),activeUser(){
 
-  /*  Session::Session(const std::string &configFilePath) {
         std::ifstream i("../config1.json");
         json j;
         i >> j;
@@ -19,13 +23,21 @@ using json = nlohmann::json;
         for (int i = 0; i < j.size(); i++) {
             long id = i;
             const std::string &name = j["movies"][i]["name"];
+            cout << name;
             int length = j["movies"][i]["length"];
+            cout << length;
             const std::vector<std::string> &tags = j["movies"][i]["tags"];
-            content.push_back(new Movie(i, name, length, tags));
+            string s="[";
+            for (vector<string>::const_iterator it = tags.begin(); it !=  tags.end(); it++)
+                s=s+(*it)+",";
+            cout << s <<"]"<<endl;
+          //  content.push_back(new Watchabl(i, length, tags));
+
 
         }
-    }*/
-  Session::Session(const std::string &configFilePath) {}
+  }
+
+
 
     Session::~Session() {
 
--- src/Watchable.cpp
+++ src/Watchable.cpp
@@ -1,11 +1,28 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include "../include/Watchable.h"
-
-Watchable::Watchable(long id, int length, const std::vector<std::string>& tags) : id(id),length(length),tags(tags){}
-virtual std::string toString() const {
-
+using namespace std;
+Watchable::Watchable(long id, int length, const vector<string>& tags) : id(id),length(length),tags(tags){}
+long Watchable:: getID() const {
+    return id ;
 }
-long getID(){
-    return this->id;
+Watchable::~Watchable(){
 }
+
+int Watchable::getLength() const{
+    return length ;
+}
+vector<string> Watchable::getTags() const{
+    return tags ;
+}
+
+
+ string Watchable:: toString() const {// virtual
+     string s="";
+     for (vector<string>::const_iterator it = tags.begin(); it != tags.end(); it++)
+         s=s+(*it)+",";
+
+    cout<<id<<". "<<length<<" minutes"<<"["<<s.substr(0,s.length()-1)<<"]";
+}
+
