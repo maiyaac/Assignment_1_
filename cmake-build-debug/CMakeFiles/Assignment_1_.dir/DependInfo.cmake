54c77c780a25c9f1eeaaf346894726fc29c65c69
edensalansky <eden.salansky@gmail.com>
2019-11-28 15:59:28 +0200

    Merge branch 'new' of /users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_ with conflicts.

--- cmake-build-debug/CMakeFiles/Assignment_1_.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Assignment_1_.dir/DependInfo.cmake
@@ -4,11 +4,25 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/src/Episode.cpp" "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Episode.cpp.o"
-  "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/src/Movie.cpp" "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Movie.cpp.o"
-  "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/src/Session.cpp" "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Session.cpp.o"
-  "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/src/Watchable.cpp" "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Watchable.cpp.o"
-  "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/src/main.cpp" "/users/studs/bsc/2020/maiy/CLionProjects/Ass1/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/main.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/Action.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Action.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/ChangeActiveUser.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/ChangeActiveUser.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/CreateUser.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/CreateUser.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/DeleteUser.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/DeleteUser.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/DuplicateUser.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/DuplicateUser.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/Episode.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Episode.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/Exit.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Exit.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/GenreRecommenderUser.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/GenreRecommenderUser.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/LengthRecommenderUser.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/LengthRecommenderUser.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/Movie.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Movie.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/PrintActionsLog.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/PrintActionsLog.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/PrintContentList.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/PrintContentList.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/PrintWatchHistory.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/PrintWatchHistory.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/RerunRecommenderUser.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/RerunRecommenderUser.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/Session.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Session.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/User.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/User.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/Watch.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Watch.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/Watchable.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/Watchable.cpp.o"
+  "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/src/main.cpp" "/users/studs/bsc/2020/edensal/CLionProjects/Assignment_1_/cmake-build-debug/CMakeFiles/Assignment_1_.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
